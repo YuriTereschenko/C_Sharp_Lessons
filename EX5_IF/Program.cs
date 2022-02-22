2d2ee896bc656c33d07664151774a38c8e3738ad
Юрий Терещенко <yuri.tereschenko@icloud.com>
2022-02-22 23:10:45 +0500

    new exampl with conditionals

---
+++ EX5_IF/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "иван")
+{
+    Console.WriteLine("Какой приятный человек!");
+}
+else
+{
+    Console.WriteLine("Привет " + username);
+}
